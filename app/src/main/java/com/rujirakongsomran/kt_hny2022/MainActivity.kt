926bf6ab33f0a5461aad878d7a3cf07f047ed4bf
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-01-01 14:41:09 +0700

    Add photo and snowfall

--- app/src/main/java/com/rujirakongsomran/kt_hny2022/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_hny2022/MainActivity.kt
@@ -2,10 +2,25 @@ package com.rujirakongsomran.kt_hny2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideSystemBars()
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun hideSystemBars() {
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
